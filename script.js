c9a38da3baf6c84ec550d0da39e88d01c965c374
Rakesharunachalam <112373658+Rakesharunachalam@users.noreply.github.com>
2022-12-28 15:09:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const btn =document.getElementById("btn");
+btn.style.backgroundColor="red";
+console.log("jbkj")
+
+btn.addEventListener('click', doit)
+function doit(){
+
+alert("sucess");
+}
